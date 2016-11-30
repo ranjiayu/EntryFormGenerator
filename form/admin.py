3a38b51a1ced173be0753b7d374cbe75a5ea8531
ranjiayu <649482260@qq.com>
2016-11-30 11:06:27 +0800

    add admin

--- form/admin.py
+++ form/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from models import Form,Key,KeyContent
 # Register your models here.
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('title','author','password','create_time')
+
+class KeyAdmin(admin.ModelAdmin):
+    list_display = ('form','keyLabel','keyType')
+
+class KeyContentAdmin(admin.ModelAdmin):
+    list_display = ('key','content','create_time')
+
+admin.site.register(Form,FormAdmin)
+admin.site.register(Key,KeyAdmin)
+admin.site.register(KeyContent,KeyContentAdmin)
